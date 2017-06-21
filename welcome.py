1f2e36bb647bc0fc90fe517fbff7d55e42e00f3c
Conner Russell <conner.cj.russell@gmail.com>
2017-06-21 10:19:03 +0100

    Add zip upload
    - Save upload to new upload folder
    - Redirect after upload
    - Filter unwanted files
    - Remove unwanted app routes

--- welcome.py
+++ welcome.py
@@ -16,13 +16,21 @@ import json
 from os.path import join, dirname
 from os import environ, getenv, listdir, remove
 from watson_developer_cloud import VisualRecognitionV3  
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_from_directory, redirect, url_for
 from werkzeug import secure_filename
-import watsonClassify
+#import watsonClassify
+
+UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSIONS = set(['jpg', 'jpeg', 'zip'])
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 visual_recognition = VisualRecognitionV3(VisualRecognitionV3.latest_version, api_key="3722ed0d4950e9c3c3c187a471043b264b2de23c")
+
+def allowed_file(filename):
+    return '.' in filename and \
+        filename.rsplit('.',1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/')
 def Welcome():
@@ -45,24 +53,23 @@ def upload_image():
         return 'there was a problem sending the file'
         #watsonClassify.main(secure_filename(f.filename))
 
-@app.route('/myapp')
-def WelcomeToMyapp():
-    return 'Welcome again to my app running on Bluemix!'
+@app.route('/zip_upload', methods = ['GET', 'POST'])
+def upload_zip():
+    if request.method == 'POST':
+        f = request.files['file']
+        if f.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if f and allowed_file(f.filename) :
+            print 'Made it to here'
+            filename = secure_filename(f.filename)
+            f.save(join(app.config['UPLOAD_FOLDER'], filename))
+            print 'And here'
+            return redirect(url_for('uploaded_file', filename = filename))
 
-@app.route('/api/people')
-def GetPeople():
-    list = [
-        {'name': 'John', 'age': 28},
-        {'name': 'Bill', 'val': 26}
-    ]
-    return jsonify(results=list)
-
-@app.route('/api/people/<name>')
-def SayHello(name):
-    message = {
-        'message': 'Hello ' + name
-    }
-    return jsonify(results=message)
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return 'Hello World! Thanks for giving me ' + filename
 
 port = getenv('PORT', '5000')
 if __name__ == "__main__":
