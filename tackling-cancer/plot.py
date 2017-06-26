128b87e1d6e5a177f798e79b6cad68999383fd20
cadet1 <cadet1@etss-MBP-2.lan>
2017-06-26 16:22:54 +0100

    modified to function to pass through parameters

--- tackling-cancer/plot.py
+++ tackling-cancer/plot.py
@@ -1,18 +1,19 @@
 import matplotlib.pyplot as plt
 
-plt.rcParams["font.family"] = "Comic Sans MS"
-fig = plt.figure()
-fig.patch.set_facecolor('white')
-fig.canvas.set_window_title('Cancer Chart')
+def plotfunc (sometuple):
+    plt.rcParams["font.family"] = "Comic Sans MS"
+    fig = plt.figure()
+    fig.patch.set_facecolor('white')
+    fig.canvas.set_window_title('Cancer Chart')
 
-blood = 1
-cancer = 3
-other = 1
+    blood = sometuple[0]
+    cancer = sometuple[1]
+    other = sometuple[2]
 
-slices = [blood, cancer, other]
-activities = ['Blood', 'Cancer', 'Other']
-cols = ['r', 'm', '#D3D3D3']
+    slices = [blood, cancer, other]
+    activities = ['Blood', 'Cancer', 'Other']
+    cols = ['r', 'm', '#D3D3D3']
 
-plt.pie(slices, labels=activities, colors = cols, startangle=90, shadow = True, explode=(0,0.15,0), autopct='%1.1f%%')
-plt.title('Cancer Chart')
-plt.show()
+    plt.pie(slices, labels=activities, colors = cols, startangle=90, shadow = True, explode=(0,0.15,0), autopct='%1.1f%%')
+    plt.title('Cancer Chart')
+    plt.show()
